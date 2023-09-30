7f0d6da8e3f3dbfda45cc4e2dd06c26c642d1354
LubovLisenko <lubovlisenko1985@gmail.com>
2023-09-30 19:09:34 +0200

    hw16

---
+++ src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.stream.IntStream;
+
+public class SumCalculator {
+    public int sum(int n) {
+        int sumCalc = 0;
+        if (n == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i <= n; i++) {
+            sumCalc =sumCalc + i;
+        }
+
+        return IntStream.rangeClosed(0,n).sum();
+
+    }
+}
---
+++ src/test/java/TestSumCalculator.java
@@ -0,0 +1,35 @@
+import org.example.SumCalculator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSumCalculator {
+    private SumCalculator sumCalculator;
+
+    @BeforeEach
+    public void beforeEach() {
+        sumCalculator = new SumCalculator();
+    }
+    @Test
+    public void testThat1InputHandledCorrectly(){
+        int actual = sumCalculator.sum(1);
+        int expected = 1;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testThat3InputHandledCorrectly(){
+        int actual = sumCalculator.sum(3);
+        int expected = 6;
+        Assertions.assertEquals(expected,sumCalculator.sum(3));
+    }
+
+
+    @Test
+    public void testThatNegativeInputHandledCorrectly(){
+        Assertions.assertThrows(IllegalArgumentException.class, () ->new SumCalculator().sum(0));
+    }
+
+}
